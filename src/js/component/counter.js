38107ebb971f444024b8e75c6baf1b6ed901b5fa
Marcelo C Alves <marcelocalves@live.com>
2021-07-03 16:48:42 +0000

    Update home.js and counter.js with parameter

--- src/js/component/counter.js
+++ src/js/component/counter.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
-export const Counter1 = () => {
+const Counter = ({ time }) => {
 	const [count, setCount] = useState(0);
 
 	if (count === 10) {
@@ -10,7 +11,7 @@ export const Counter1 = () => {
 	useEffect(() => {
 		setInterval(() => {
 			setCount(count => count + 1);
-		}, 1000);
+		}, time);
 	}, []);
 	return (
 		<span
@@ -28,138 +29,8 @@ export const Counter1 = () => {
 	);
 };
 
-export const Counter2 = () => {
-	const [count, setCount] = useState(0);
-
-	if (count === 10) {
-		setCount(0);
-	}
-
-	useEffect(() => {
-		setInterval(() => {
-			setCount(count => count + 1);
-		}, 10000);
-	}, []);
-	return (
-		<span
-			style={{
-				background: "rgb(41,41,41)",
-				padding: "0px 25px",
-				margin: "5px 10px",
-				border: "1px solid rgb(61, 61, 61)",
-				borderRadius: "15px",
-				borderLeft: "5px solid rgb(61, 61, 61)",
-				borderRight: "5px solid rgb(61, 61, 61)"
-			}}>
-			{count}
-		</span>
-	);
+Counter.propTypes = {
+	time: PropTypes.time
 };
-export const Counter3 = () => {
-	const [count, setCount] = useState(0);
 
-	if (count === 10) {
-		setCount(0);
-	}
-
-	useEffect(() => {
-		setInterval(() => {
-			setCount(count => count + 1);
-		}, 100000);
-	}, []);
-	return (
-		<span
-			style={{
-				background: "rgb(41,41,41)",
-				padding: "0px 25px",
-				margin: "5px 10px",
-				border: "1px solid rgb(61, 61, 61)",
-				borderRadius: "15px",
-				borderLeft: "5px solid rgb(61, 61, 61)",
-				borderRight: "5px solid rgb(61, 61, 61)"
-			}}>
-			{count}
-		</span>
-	);
-};
-export const Counter4 = () => {
-	const [count, setCount] = useState(0);
-
-	if (count === 10) {
-		setCount(0);
-	}
-
-	useEffect(() => {
-		setInterval(() => {
-			setCount(count => count + 1);
-		}, 1000000);
-	}, []);
-	return (
-		<span
-			style={{
-				background: "rgb(41,41,41)",
-				padding: "0px 25px",
-				margin: "5px 10px",
-				border: "1px solid rgb(61, 61, 61)",
-				borderRadius: "15px",
-				borderLeft: "5px solid rgb(61, 61, 61)",
-				borderRight: "5px solid rgb(61, 61, 61)"
-			}}>
-			{count}
-		</span>
-	);
-};
-export const Counter5 = () => {
-	const [count, setCount] = useState(0);
-
-	if (count === 10) {
-		setCount(0);
-	}
-
-	useEffect(() => {
-		setInterval(() => {
-			setCount(count => count + 1);
-		}, 10000000);
-	}, []);
-	return (
-		<span
-			style={{
-				background: "rgb(41,41,41)",
-				padding: "0px 25px",
-				margin: "5px 10px",
-				border: "1px solid rgb(61, 61, 61)",
-				borderRadius: "15px",
-				borderLeft: "5px solid rgb(61, 61, 61)",
-				borderRight: "5px solid rgb(61, 61, 61)"
-			}}>
-			{count}
-		</span>
-	);
-};
-export const Counter6 = () => {
-	const [count, setCount] = useState(0);
-
-	if (count === 10) {
-		setCount(0);
-	}
-
-	useEffect(() => {
-		setInterval(() => {
-			setCount(count => count + 1);
-		}, 100000000);
-	}, []);
-	return (
-		<span
-			style={{
-				background: "rgb(41,41,41)",
-				padding: "0px 25px",
-				margin: "5px 10px",
-				border: "1px solid rgb(61, 61, 61)",
-				borderRadius: "15px",
-				borderLeft: "5px solid rgb(61, 61, 61)",
-				borderRight: "5px solid rgb(61, 61, 61)"
-			}}>
-			{count}
-		</span>
-	);
-};
+export default Counter;
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
-import {
-	Counter1,
-	Counter2,
-	Counter3,
-	Counter4,
-	Counter5,
-	Counter6
-} from "./counter";
+// import {
+// 	Counter1,
+// 	Counter2,
+// 	Counter3,
+// 	Counter4,
+// 	Counter5,
+// 	Counter6
+// } from "./counter";
+import Counter from "./counter";
 import Clock from "./clock";
 
 //create your first component
 export function Home() {
+	let time = [1000, 10000, 100000, 1000000, 10000000, 100000000];
 	return (
 		<div
 			style={{
@@ -21,12 +23,12 @@ export function Home() {
 				textAlign: "center"
 			}}>
 			<Clock />
-			<Counter6 />
-			<Counter5 />
-			<Counter4 />
-			<Counter3 />
-			<Counter2 />
-			<Counter1 />
+			<Counter time={time[5]} />
+			<Counter time={time[4]} />
+			<Counter time={time[3]} />
+			<Counter time={time[2]} />
+			<Counter time={time[1]} />
+			<Counter time={time[0]} />
 		</div>
 	);
 }
